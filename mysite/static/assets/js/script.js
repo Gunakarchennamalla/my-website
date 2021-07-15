444252ef2a80e39f0b7d7fd3d11b0bfb070caaaf
Anju Gunakar Chennamalla <79681539+Gunakarchennamalla@users.noreply.github.com>
2021-07-15 12:08:33 +0530

    Update script.js
    floating button

--- mysite/static/assets/js/script.js
+++ mysite/static/assets/js/script.js
@@ -12,27 +12,62 @@ let themedots = document.getElementsByClassName('theme-dot')
 for(var i=0; themedots.length > i; i++){
     themedots[i].addEventListener('click',function(){
         var mode = this.dataset.mode
-        console.log('clicked item:', mode)
+        //console.log('clicked item:', mode)
         setTheme(mode)
     })
 }
 
 function setTheme(mode){
     if(mode == 'light'){
-       document.getElementById('theme-style').href = 'static/assets/css/default.css'
+       document.getElementById('theme-style').href = 'default.css'
     }
 
     if(mode == 'blue'){
-        document.getElementById('theme-style').href = 'static/assets/css/blue.css'
+        document.getElementById('theme-style').href = 'blue.css'
     }
 
     if(mode == 'green'){
-        document.getElementById('theme-style').href = 'static/assets/css/green.css'
+        document.getElementById('theme-style').href = 'green.css'
     }
 
     if(mode == 'purple'){
-        document.getElementById('theme-style').href = 'static/assets/css/purple.css'
+        document.getElementById('theme-style').href = 'purple.css'
     }
 
     localStorage.setItem('theme', mode)
 }
+
+
+
+
+
+
+
+            window.onload = function(){
+
+	const trigger = document.querySelector("menu > .trigger");
+	trigger.addEventListener('click', (e) => {
+ 		 e.currentTarget.parentElement.classList.toggle("open");
+	});
+
+          /*Scroll to top when arrow up clicked BEGIN*/
+
+        $(window).scroll(function() {
+            var height = $(window).scrollTop();
+            if (height > 100) {
+                $('#back2Top').fadeIn();
+            } else {
+                $('#back2Top').fadeOut();
+            }
+        });
+        $(document).ready(function() {
+            $("#back2Top").click(function(event) {
+                event.preventDefault();
+                $("html, body").animate({ scrollTop: 0 }, "slow");
+                return false;
+            });
+
+        });
+
+        }; 
+         /*Scroll to top when arrow up clicked END*/
